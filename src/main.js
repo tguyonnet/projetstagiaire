1323d3b8e0bd7ea66b049fd74c60bdc626062bd2
Théo <theo@guyonnet.me>
2020-10-20 13:18:54 +0200

    :sparkles: Ajout de pouchdb

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router.js'
+import PouchDB from 'pouchdb'
 
 Vue.config.productionTip = false
+
+var db = new PouchDB('madera');
+Vue.prototype.$db = db
 
 new Vue({
   router,
   render: h => h(App),
+  data: {
+    // db: db,
+  }
 }).$mount('#app')
--- src/router.js
+++ src/router.js
@@ -8,7 +8,6 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-    //   component: Home
     },
     {
       path: '/clients',
