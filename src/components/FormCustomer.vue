b4f3c42c21c25b3f894f499bafca3d04b37ddee3
Théo <theo@guyonnet.me>
2020-10-21 11:02:44 +0200

    :bug: customer add

--- src/components/FormCustomer.vue
+++ src/components/FormCustomer.vue
@@ -4,29 +4,29 @@
       <v-form v-model="valid">
         <v-container>
           <v-row>
-            <v-col cols="12" md="6">
+            <v-col md="6">
               <v-text-field v-model="dataForm.name" label="Nom" required></v-text-field>
             </v-col>
-            <v-col cols="12" md="6">
+            <v-col md="6">
               <v-text-field v-model="dataForm.firstName" label="Prénom" required></v-text-field>
             </v-col>
           </v-row>
           <v-row>
-            <v-col cols="12" md="5">
+            <v-col md="5">
               <v-text-field v-model="dataForm.address" label="Adresse" required></v-text-field>
             </v-col>
-            <v-col cols="12" md="4">
+            <v-col md="4">
               <v-text-field v-model="dataForm.city" label="Ville" required></v-text-field>
             </v-col>
-            <v-col cols="12" md="3">
+            <v-col md="3">
               <v-text-field v-model="dataForm.postCode" label="Code postal" required></v-text-field>
             </v-col>
           </v-row>
           <v-row>
-            <v-col cols="12" md="6">
+            <v-col md="6">
               <v-text-field v-model="dataForm.email" :rules="emailRules" label="E-mail" required></v-text-field>
             </v-col>
-            <v-col cols="12" md="6">
+            <v-col md="6">
               <v-text-field v-model="dataForm.phone" :rules="phoneRules" label="Téléphone" required></v-text-field>
             </v-col>
 
