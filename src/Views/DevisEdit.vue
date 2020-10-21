2b5b49655b618c62cc75f35d84e784d5f587564b
Kevin <kevin/nave@viacesi.fr>
2020-10-21 16:57:24 +0200

    Ajout arbre devis

--- src/Views/DevisEdit.vue
+++ src/Views/DevisEdit.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
+      <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><router-link to="/">Home</router-link></li>
+      <li class="breadcrumb-item active" aria-current="page">Devis</li>
+    </ol>
+  </nav>
     <h1>Gestion des devis</h1>
   </div>
 </template>
